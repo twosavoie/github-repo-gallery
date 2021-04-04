cf61507cdc8d0ea56fbded0c559c0be7c53752e5
lsavoie <twosavoie@gmail.com>
2021-04-04 11:40:17 -0500

    Fetch Repo Data

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 const overview = document.querySelector(".overview");
 const username = "twosavoie";
+const repoList = document.querySelector(".repo-list");
 
 const gitUserInfo = async function () {
   const userInfo = await fetch(`https://api.github.com/users/${username}`);
   const data = await userInfo.json();
   displayUserInfo(data);
-//  console.log(data);
 };
 
 gitUserInfo();
@@ -25,4 +25,22 @@ const displayUserInfo = function (data) {
     </div>
   `;
   overview.append(div);
-}; 
+  gitRepos();
+};
+
+const gitRepos = async function () {
+  const fetchRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+  const repoData = await fetchRepos.json();
+  displayRepos(repoData);
+  // console.log(repoData);
+};
+// gitRepos();
+
+const displayRepos = function (repos) {
+  for (const repo of repos) {
+    const repoItem = document.createElement("li");
+    repoItem.classList.add("repo");
+    repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+    repoList.append(repoItem);
+  }
+};
